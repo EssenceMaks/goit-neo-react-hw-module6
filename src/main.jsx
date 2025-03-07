1c58595d95bdb2fecd150dfeb23256d3e7100b78
EssenceMaks <makski.wish@gmail.com>
2025-03-07 11:37:21 +0200

    redux ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import { ThemeProvider } from './context/ThemeContext';
-import './index.css'
-import App from './App.jsx'
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
